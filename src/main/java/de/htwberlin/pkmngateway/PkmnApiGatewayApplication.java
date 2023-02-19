a1b3791142d91b61f7ed6510e47cf107c2695aa9
Alexander Mai <83711446+GodPepe7@users.noreply.github.com>
2023-02-19 17:29:05 +0100

    PKMN-55 api gateway implementation

---
+++ src/main/java/de/htwberlin/pkmngateway/PkmnApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.pkmngateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkmnApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkmnApiGatewayApplication.class, args);
+	}
+
+}
